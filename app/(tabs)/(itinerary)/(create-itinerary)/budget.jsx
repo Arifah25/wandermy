6ab476b3382b8c5d84c8ad8d6952e6b867d48bee
umiar <umiariffah@gmail.com>
2024-12-31 14:27:11 +0800

    Revert "Add react-native-picker-select for budget and traveler selection"
    This reverts commit 43ddec73c345be6c617d5b0205be0193a749a746.

--- app/(tabs)/(itinerary)/(create-itinerary)/budget.jsx
+++ app/(tabs)/(itinerary)/(create-itinerary)/budget.jsx
@@ -1,70 +1,58 @@
-import { View, Text } from 'react-native';
+import { View, Text, FlatList } from 'react-native'
 import React, { useContext, useEffect, useState } from 'react';
-import { Button } from '../../../../components';
-import { useRouter } from 'expo-router';
+import { Button, OptionCard } from '../../../../components';
+import { useNavigation, useRouter } from 'expo-router';
 import { CreateItineraryContext } from '../../../../context/CreateItineraryContext';
 import { SelectBudgetList } from '../../../../constants/option';
-import RNPickerSelect from 'react-native-picker-select';
+import { TouchableOpacity } from 'react-native';
 
 const SelectBudget = () => {
-  const router = useRouter();
-  const [selectedBudget, setSelectedBudget] = useState();
-  const { itineraryData, setItineraryData } = useContext(CreateItineraryContext);
+    const router = useRouter();
 
-  useEffect(() => {
-    if (selectedBudget) {
-      setItineraryData({
-        ...itineraryData,
-        budget: selectedBudget,
-      });
-    }
-  }, [selectedBudget]);
+    const [selectedBudget, setSelectedBudget] = useState();
+    const {itineraryData, setItineraryData} = useContext(CreateItineraryContext);
 
-  return (
-    <View className="bg-white flex-1 p-5 h-full items-center justify-start">
-      <Text className="text-3xl font-ksemibold text-center">
+    useEffect(() =>{
+        selectedBudget&&setItineraryData({
+            ...itineraryData,
+          budget: selectedBudget
+        })
+      },[selectedBudget])
+
+    return (
+    <View
+    className="bg-white flex-1 p-5 h-full items-center justify-start"
+    >
+      <Text
+      className="text-3xl font-ksemibold text-center"
+      >
         Choose your spending habits
       </Text>
       <View className="w-full mt-7">
-        <RNPickerSelect
-          onValueChange={(value) => setSelectedBudget(value)}
-          items={SelectBudgetList.map((item) => ({
-            label: `${item.icon} ${item.title} - ${item.desc}`,
-            value: item.title,
-          }))}
-          placeholder={{ label: 'Select your budget', value: null }}
-          style={{
-            inputIOS: {
-              fontSize: 16,
-              paddingVertical: 12,
-              paddingHorizontal: 10,
-              borderWidth: 1,
-              borderColor: 'gray',
-              borderRadius: 4,
-              color: 'black',
-              paddingRight: 30, // to ensure the text is never behind the icon
-            },
-            inputAndroid: {
-              fontSize: 16,
-              paddingHorizontal: 10,
-              paddingVertical: 8,
-              borderWidth: 0.5,
-              borderColor: 'purple',
-              borderRadius: 8,
-              color: 'black',
-              paddingRight: 30, // to ensure the text is never behind the icon
-            },
-          }}
+        <FlatList
+        data={SelectBudgetList}
+        renderItem={({item, index}) => (
+          <View className="items-center">
+            <TouchableOpacity
+            onPress={() => setSelectedBudget(item)}
+            >
+              <OptionCard 
+              option={item}
+              selected={selectedBudget}
+              />
+            </TouchableOpacity>
+          </View>
+        )}
         />
       </View>
       <Button
-        title="Next"
-        textColor="text-white"
-        style="bg-primary w-3/4 mt-5"
-        handlePress={() => router.push('(tabs)/(itinerary)/(create-itinerary)/reviewdetails')}
+      title="Next"
+      textColor="text-white"
+      style="bg-primary w-3/4 mt-5"
+      handlePress={() => router.push('(tabs)/(itinerary)/(create-itinerary)/reviewdetails')}
       />
     </View>
-  );
-};
+    )
+}
 
-export default SelectBudget;
+export default SelectBudget
--- app/(tabs)/(itinerary)/(create-itinerary)/new.jsx
+++ app/(tabs)/(itinerary)/(create-itinerary)/new.jsx
@@ -1,193 +1,118 @@
-import { View, Text, TextInput } from 'react-native';
-import React, { useContext, useEffect, useState } from 'react';
-import { useRouter } from 'expo-router';
+import { View, Text, TextInput } from 'react-native'
+import React, { useContext, useEffect, useState } from 'react'
+import { useRouter } from 'expo-router'
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Button, DateField, SearchPlace } from '../../../../components';
 import 'react-native-get-random-values';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import { CreateItineraryContext } from '../../../../context/CreateItineraryContext';
-import CalendarPicker from 'react-native-calendar-picker';
-import RNPickerSelect from 'react-native-picker-select';
+import CalendarPicker from "react-native-calendar-picker"
 import moment from 'moment';
-import { SelectBudgetList, SelectTravelList } from '../../../../constants/option';
 
 const NewItinerary = () => {
-  const router = useRouter();
-  const { itineraryData, setItineraryData } = useContext(CreateItineraryContext);
+    const router = useRouter();
+    const { itineraryData, setItineraryData } = useContext(CreateItineraryContext);
 
-  // Initialize state variables for attributes in event
-  const [tripName, setTripName] = useState('');
-  const [startDate, setStartDate] = useState();
-  const [endDate, setEndDate] = useState();
-  const [selectedBudget, setSelectedBudget] = useState();
-  const [selectedTraveler, setSelectedTraveler] = useState();
+    //Initialize state variables for attributes in event 
+    const [tripName, setTripName] = useState(''); 
+    const [startDate, setStartDate] = useState();
+    const [endDate, setEndDate] = useState();
 
-  const onDateChange = (date, type) => {
-    if (type == 'START_DATE') {
-      setStartDate(moment(date));
-    } else if (type == 'END_DATE') {
-      setEndDate(moment(date));
+    const onDateChange = (date, type) => {
+      if (type == 'START_DATE')
+      {
+        setStartDate(moment(date));
+      }
+      else if (type == 'END_DATE')
+      {
+        setEndDate(moment(date));
+      }
     }
-  };
 
-  const OnDateSelectionContinue = () => {
-    const totalNoOfDays = endDate.diff(startDate, 'days');
-    setItineraryData({
-      ...itineraryData,
-      startDate: startDate,
-      endDate: endDate,
-      totalNoOfDays: totalNoOfDays + 1,
-    });
-    router.push('(tabs)/(itinerary)/(create-itinerary)/traveler');
-  };
-
-  const handleTripNameChange = (value) => {
-    setTripName(value);
-    setItineraryData((prevData) => ({
-      ...prevData,
-      tripName: value,
-    }));
-  };
-
-  useEffect(() => {
-    if (selectedBudget) {
+    const OnDateSelectionContinue = () => {
+      const totalNoOfDays = endDate.diff(startDate, 'days');
+      console.log(totalNoOfDays+1);
+      setItineraryData({
+        ...itineraryData,
+        startDate: startDate,
+        endDate: endDate,
+        totalNoOfDays: totalNoOfDays+1
+      });
+      console.log(itineraryData);
+      router.push('(tabs)/(itinerary)/(create-itinerary)/traveler')
+    }
+    
+    const handleTripNameChange = (value) => {
+      setTripName(value);
       setItineraryData((prevData) => ({
         ...prevData,
-        budget: selectedBudget,
+        tripName: value,
       }));
-    }
-  }, [selectedBudget]);
-
-  useEffect(() => {
-    if (selectedTraveler) {
-      setItineraryData((prevData) => ({
-        ...prevData,
-        traveler: selectedTraveler,
-      }));
-    }
-  }, [selectedTraveler]);
+    };
 
   return (
-    <View className="bg-white flex-1 p-5 h-full items-center justify-start">
-      <ScrollView></ScrollView>
-      <View className="w-full items-center">
-        <Text className="font-kregular text-xl">Select A Destination</Text>
-        <View className="w-full h-14 items-center mt-2 z-50">
-          <SearchPlace />
-        </View>
-      </View>
-      <View className="w-full items-center mt-5">
-        <Text className="font-kregular text-xl">Trip Name</Text>
-        <View className="w-11/12 border-2 border-secondary rounded-md mt-2 h-14 p-3 justify-center focus:border-black">
+    <View
+    className="bg-white flex-1 p-5 h-full items-center justify-start"
+    >
+      <View
+      className="w-full items-center">
+        <Text
+        className="font-kregular text-xl">
+          Select A Destination
+        </Text>
+        <View className="w-full h-14 items-center mt-2 z-50"> 
+          <SearchPlace 
+          />  
+        </View>      
+      </View> 
+      <View
+      className="w-full items-center mt-5">
+        <Text
+        className="font-kregular text-xl">
+          Trip Name
+        </Text>
+        <View
+        className="w-11/12 border-2 border-secondary rounded-md mt-2 h-14 p-3 justify-center focus:border-black">
           <TextInput
-            className="font-kregular text-base h-14"
-            placeholder="e.g, Semester Break Trip"
-            placeholderTextColor="#7E6C6C"
-            value={tripName}
-            onChangeText={handleTripNameChange}
+          className="font-kregular text-base h-14 "
+          placeholder="e.g, Semester Break Trip"
+          placeholderTextColor="#7E6C6C"
+          value={tripName}
+          onChangeText={handleTripNameChange}
           />
         </View>
       </View>
-      <View className="w-full items-center mt-5">
-        <Text className="font-kregular text-xl">Budget</Text>
-        <View className="w-11/12 border-2 border-secondary rounded-md mt-2 h-14 p-3 justify-center focus:border-black">
-          <RNPickerSelect
-            onValueChange={(value) => setSelectedBudget(value)}
-            items={SelectBudgetList.map((item) => ({
-              label: `${item.title} - ${item.desc}`,
-              value: item.title,
-            }))}
-            placeholder={{ label: 'Select your budget', value: null }}
-            style={{
-              inputIOS: {
-                fontFamily: 'Kanit',
-                fontSize: 16,
-                paddingVertical: 12,
-                paddingHorizontal: 10,
-                borderWidth: 1,
-                borderColor: 'gray',
-                borderRadius: 4,
-                color: 'black',
-                paddingRight: 30, // to ensure the text is never behind the icon
-              },
-              inputAndroid: {
-                
-                fontSize: 16,
-                paddingHorizontal: 10,
-                paddingVertical: 8,
-                borderWidth: 0.5,
-                borderColor: 'purple',
-                borderRadius: 8,
-                color: 'black',
-                paddingRight: 30, // to ensure the text is never behind the icon
-              },
-            }}
-          />
-        </View>
-      </View>
-      <View className="w-full items-center mt-5">
-        <Text className="font-kregular text-xl">Traveler</Text>
-        <View className="w-11/12 border-2 border-secondary rounded-md mt-2 h-14 p-3 justify-center focus:border-black">
-          <RNPickerSelect
-            onValueChange={(value) => setSelectedTraveler(value)}
-            items={SelectTravelList.map((item) => ({
-              label: `${item.title} - ${item.desc}`,
-              value: item.title,
-            }))}
-            placeholder={{ label: 'Select your traveler', value: null }}
-            style={{
-              inputIOS: {
-                fontSize: 16,
-                paddingVertical: 12,
-                paddingHorizontal: 10,
-                borderWidth: 1,
-                borderColor: 'gray',
-                borderRadius: 4,
-                color: 'black',
-                paddingRight: 30, // to ensure the text is never behind the icon
-              },
-              inputAndroid: {
-                fontSize: 16,
-                paddingHorizontal: 10,
-                paddingVertical: 8,
-                borderWidth: 0.5,
-                borderColor: 'purple',
-                borderRadius: 8,
-                color: 'black',
-                paddingRight: 30, // to ensure the text is never behind the icon
-              },
-            }}
-          />
-        </View>
-      </View>
-      <View className="justify-center items-center mt-7">
-        <Text className="font-kregular text-xl">Travel Dates</Text>
+      <View className="justify-center items-center mt-7 ">
+        <Text
+        className="font-kregular text-xl"
+        >Travel Dates</Text>
       </View>
       <View className="mt-2">
-        <CalendarPicker
-          onDateChange={onDateChange}
-          width={345}
-          height={345}
-          allowRangeSelection={true}
-          minDate={new Date()}
-          minRangeDuration={1}
-          maxRangeDuration={5}
-          selectedRangeStyle={{
-            backgroundColor: 'salmon',
-          }}
-          selectedDayTextStyle={{
-            color: 'white',
-          }}
-        />
-      </View>
-      <Button
-        title="Start Crafting"
-        textColor="text-white"
-        style="bg-primary my-10 w-3/5"
-        handlePress={OnDateSelectionContinue}
-      />
+        <CalendarPicker 
+        onDateChange={onDateChange}
+        width={345}
+        height={345}
+        allowRangeSelection={true}
+        minDate={new Date()}
+        minRangeDuration={1}
+        maxRangeDuration={5}
+        selectedRangeStyle={{
+          backgroundColor: 'salmon'
+        }}
+        selectedDayTextStyle={{
+          color: 'white'
+        }}
+         />
+      </View>    
+      <Button 
+      title="Start Crafting"
+      textColor="text-white"
+      style="bg-primary my-10 w-3/5"
+      handlePress={OnDateSelectionContinue}
+      // handlePress={() => router.navigate('(tabs)/(itinerary)/')}
+      />        
     </View>
-  );
-};
+  )
+}
 
-export default NewItinerary;
+export default NewItinerary
